1a0d1802d9a631dea52b2233ad84bfa48abb93a1
Wanda Gleason <oglissane@gmail.com>
2021-03-07 22:02:02 -0500

    Added code for app.get - return index.html file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,6 +13,10 @@ module.exports = (app) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
+  // return index.html file
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
   // If no matching route is found default to home
   app.get('*', (req, res) => {
