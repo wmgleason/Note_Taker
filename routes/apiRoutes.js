17dcfd9ac32cb3d36b2605de5e08fe1f870ede91
Wanda Gleason <oglissane@gmail.com>
2021-03-07 22:21:44 -0500

    Added delete note code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,19 @@
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+const path = require('path');
+const fs = require('fs');
+let noteCount = 1;
 
-const tableData = require('../data/tableData');
-const waitListData = require('../data/waitinglistData');
+// LOAD DATA for notes
+const notesData = require('../data/notesData');
 
 // ROUTING
 
-module.exports = (app) => {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
+
+module.exports = (app) => {
 
   app.get('/api/notes', (req, res) => res.json(notesData));
 
@@ -40,14 +41,13 @@ module.exports = (app) => {
     // }
   });
 
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
 
-//   app.post('/api/clear', (req, res) => {
-//     // Empty out the arrays of data
-//     notesData.length = 0;
-//     waitListData.length = 0;
 
-//     res.json({ ok: true });
-//   });
-// };
+  app.post('/api/clear', (req, res) => {
+
+    notesData.length = 0;
+    waitListData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
