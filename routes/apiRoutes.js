53f0bddf06dca827728fc3db00bdfd873850a5f1
Wanda Gleason <oglissane@gmail.com>
2021-03-07 22:26:57 -0500

    Now using FS

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,21 @@ const fs = require('fs');
 let noteCount = 1;
 
 // LOAD DATA for notes
-const notesData = require('../data/notesData');
+// const notesData = require('../data/notesData');
+
+const readData = () => {
+
+  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')))
+  return notesData;
+}
+
+const writeData = (notesData) => {
+  fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData), (err) => {
+      if (err) return ({ err });
+  })
+}
+
+const newNoteId = () => noteId++;
 
 // ROUTING
 
@@ -26,28 +40,38 @@ module.exports = (app) => {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-
-  app.post('/api/notes', (req, res) => {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (notesData.length < 5) {
-      notesData.push(req.body);
-      res.json(true);
-    } 
+    // if (notesData.length < 5) {
+    //   notesData.push(req.body);
+    //   res.json(true);
+    // } 
     // else {
     //   waitListData.push(req.body);
     //   res.json(false);
     // }
-  });
+  // });
 
 
+  app.post('/api/notes', (req, res) => {
+    let notesData = readData();
+    let newNote = req.body;
+    let lastNoteID = !notesData[0] ? 0 : notesData[notesData.length - 1].id;
+    let newNoteID = lastNoteID + 1
 
-  app.post('/api/clear', (req, res) => {
+    newNote.id = newNoteID;
+    notesData.push(newNote);
+    writeData(notesData);
+    return res.json(notesData)
+})
 
-    notesData.length = 0;
-    waitListData.length = 0;
+app.delete('/api/notes/:id', (req, res) => {
+  let notesData = readData();
+  const noteId = req.params.id;
+  const newNoteData = notesData.filter((note) => note.id != noteId);
 
-    res.json({ ok: true });
-  });
-};
+  writeData(newNoteData);
+  res.send(newNoteData);
+})
+}
